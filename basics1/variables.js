10f491fd5b3893f92566daa418c11f5c36cb2469
Shobhika <134936907+shobhikaa16@users.noreply.github.com>
2024-06-10 17:11:50 +0000

    changes 1

---
+++ basics1/variables.js
@@ -0,0 +1,15 @@
+const accountId = 144537  // value cant be changed
+let accountEmail = "abc@gmail.com" // two ways to declare variables var and let 
+var accontPswrd = "12345"
+accountCity = "Bareilly" // not a good method but can possible 
+let accntState; // can or cannot use ; and this value will be undefined
+
+// accountId =346 // cant be changed
+accountEmail = "abc@gmail.com"
+accontPswrd = "1244657"
+accountCity = "Banglore"
+/*
+prefer no to use var becasue of block scope and functional scope
+ */
+
+console.table([accountId,accountEmail,accontPswrd,accountCity])
