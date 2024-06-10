b6a47545948ff83c61d8b806f35dd22f5238e356
Shobhika <134936907+shobhikaa16@users.noreply.github.com>
2024-06-10 17:33:49 +0000

    change2

---
+++ basics1/dataTypes.js
@@ -0,0 +1,30 @@
+"use  strict" //treat all JS code as newer version
+
+//alert("hello")
+//alert(3+3) not used now , we are using nodejs not browser
+
+console.log(3+3)
+let name = "shobhika"
+let age = 21
+let isLoggedIn = false
+console.log(typeof age)
+console.log(typeof null)  // object 
+console.log(typeof undefined) // undefined
+
+// primitive data types
+ // number range = 2^53
+ // bigint 
+ // string =>""
+ // boolean => true or false
+ // null  => it is a stand alone value , representation of empty thing
+ // undefined => value is not defined
+ // symbol => uniqueness
+
+//object 
+
+
+ 
+
+
+
+
