c8fb9c44e3352c3f0661a099392aba92bb27df35
Shobhika <134936907+shobhikaa16@users.noreply.github.com>
2024-06-12 17:03:22 +0000

    conversions and operations

---
+++ basics1/conversionoperation.js
@@ -0,0 +1,61 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let marks = "56"
+console.log(typeof marks)
+let valueInNumber = Number(marks)
+console.log(typeof valueInNumber)
+
+let nonNum = "33sc"  // try to pass null = 0 and undefined=Nan and boolean true =1, false =0, here
+console.log( typeof noNum)
+let num = Number(nonNum)
+console.log(typeof num)  // there is no strict check in javascript 
+
+let isloggedIn = 1 // put string also here
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn)
+// 1 = fase , true = 0
+// ""= false
+//"shobhika" = true
+
+
+let no = 44
+let stringNum = String(no)
+console.log(stringNum)
+console.log(typeof stringNum)
+
+
+// Operations
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(3+2)
+console.log(3-2)
+console.log(3*2)
+console.log(3/2)
+console.log(3%2)
+
+let str1 ="hello"
+let str2 = " shobhika"
+let str3 = str1+str2
+console.log(str3)
+
+console.log(1 +"2");
+console.log("1"+2);
+console.log("1"+2+2)
+console.log(3+2+"1")
+
+console.log(+true);
+console.log(true);  // true+ not possible
+console.log(+"");
+
+
+
+
+
+
