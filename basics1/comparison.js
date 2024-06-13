4a95f905ab97b3cb47dc7f7acc7b4b53dfeac640
Shobhika <134936907+shobhikaa16@users.noreply.github.com>
2024-06-13 19:11:04 +0000

    comparable operations

---
+++ basics1/comparison.js
@@ -0,0 +1,24 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+console.log("2">1); // automatically js convert 2 into no. 
+console.log("02">1);
+
+console.log(null>0);// false
+console.log(null==0);// false
+console.log(null>=0);// true
+// comparator converts null into a no. but compare do not conert it into a no. so ans are these
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+console.log("2" === 2); //strict check
+
+
+
+
+
