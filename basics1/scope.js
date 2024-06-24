bc98b65f62a933a1fcf08fd60d3fd9a11e0f5282
Shobhika <134936907+shobhikaa16@users.noreply.github.com>
2024-06-24 20:49:10 +0000

    scopes

--- basics1/scope.js
+++ basics1/scope.js
@@ -20,4 +20,42 @@ console.log(a) // yeh dono nhi hai apne scope ke bahr but var aagya
 console.log(c)
 
 // windows ke andar scope alg and browser ke andar alg 
+function one (){
+    const userName ="Shobhit"
 
+    function two(){  //function two is inside the one so we can access the things of one kyuki one bda hai aur twoc chota hai lekin 
+        const website ="youtube"
+        console.log(userName)
+    }
+    //console.log(website); // yeh execute nhi hoga kyuki vo us scope ke andar tha aur wahi khtm
+
+    two()
+}
+one()
+
+if(true){
+    const username ="Sapna"
+    if(username === "Sapna"){
+        const website ="youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+
+}
+// console.log(username);
+
+//++++++++++++++++++++++++++ Interesting ++++++++++++++++++++++
+
+
+addone(5)  // *important*if we decalre functions in this way then can be called before  but if we dclare them as given below then they cant be called before
+
+function addone(value){
+    return value+1;
+
+}
+
+
+const addTwo = function(num){  // this is also function but also called expression some times
+    return num+2
+}
+console.log(addTwo(5))
