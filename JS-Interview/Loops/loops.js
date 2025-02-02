07747d04f55469f59fa3e959c0416c2a0bfca43e
Shobhika <134936907+shobhikaa16@users.noreply.github.com>
2025-02-02 11:23:11 +0000

    more

---
+++ JS-Interview/Loops/loops.js
@@ -0,0 +1,74 @@
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+
+// loops in javascript to iteratie the piese of code
+// syntax for( initialisation; conditon; updation){ do somwthing here}
+
+/*
+for( let i=1; i<=5; i++){
+    console.log(i);
+}
+
+for(let i=5; i>=1; i=i-2){
+    console.log(i);
+}
+
+// print all odd nums - or use i=i+2
+for(let i=1; i<=50; i++){
+    if(i % 2 != 0){
+        console.log(i);
+    }
+}
+
+// print all even nums - or use  let i=2 and i = i+2
+for(let i=1; i<=50; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
+// table of 5
+for(let i=1; i<=10; i++){
+    console.log(i*5);
+}
+
+let n= prompt("write your number"); // anything we take in prompt is string so to convert it useparseInt
+n = parseInt(n);
+
+for(let i=1; i<=10; i++){
+    console.log(i*n);
+}
+
+*/
+
+//Nested loops
+for(let i=1; i<=3; i++){
+    console.log(`outer loop ${i}`);
+    for(let j=1; j<=3; j++){
+        console.log(j);
+    }
+}
+
+//while loops
+let i=1;
+while(i <= 5){
+    console.log(i);
+    i++;
+}
+
+let j = 5;
+while( j >= 1){
+    console.log(j);
+    j--;
+}
+
+
+//favourite Movie
+
+let favMovie = "Harry Potter"
+
+let guess = prompt(" Guess the movie - Most famous movie among children");
+while(guess != favMovie){
+    
+}
