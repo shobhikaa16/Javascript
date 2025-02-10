cca3b576f0d7fea26dfc0121b3b4dacf8c8c7b11
Shobhika <134936907+shobhikaa16@users.noreply.github.com>
2025-02-10 14:36:09 +0000

    setting up promises

---
+++ JS-Interview/CallBacks/promise.js
@@ -0,0 +1,6 @@
+// promise krta hai promise aur phir batata ki vo cheez aage jake hogi yaa nhi hogi 
+// the code we made last we will execute that with the help of promises now
+// the promises object represents the eventual completetion or failure of an asynchronous operation and its resulting value
+//Promise  object has two things (resolve and reject)
+
+// like in last internetspeed function instead of success and failure we will return the promise object 
