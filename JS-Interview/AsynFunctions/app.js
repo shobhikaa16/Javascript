c9b93db8cdcfd7119bf477eb6b521f8193e722d7
Shobhika <134936907+shobhikaa16@users.noreply.github.com>
2025-02-12 12:04:29 +0000

    changes

--- JS-Interview/AsynFunctions/app.js
+++ JS-Interview/AsynFunctions/app.js
@@ -9,9 +9,34 @@ async function greet(){
 
 greet()
 .then((result)=>{
-    console.log("primise was resolves");
-    console.log("promise was rejected with err:")
+    console.log("promise was resolves");
+    console.log("result was :",result)
 })
-.catch(err)=>{
-    console.log("promise was re")
+.catch((err)=>{
+    console.log("promise was rejected with error:",err)
+})
+
+// hum arrow funciton sko bhi async bnaskte hai 
+// let demo =async()=>{
+//     return 5;
+// }
+
+//await keyword
+// pauses the execution of its surrounding async funciton until the promise is settled (resolved or rejected)
+function getNum(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            let num = Math.floor(Math.random()*10)+1;
+            console.log(num);
+            resolve();
+        },1000)
+        
+    });
+}
+
+async function demo(){
+    await getNum(); // humne cal kia getNum ko toh abhi yeh teeno num ek saath ajaenge ek second baad, but we want that har ek second 
+    // baad msg aaye isiliye we use async await.
+    await getNum();
+    getNum(); //we have use wait so they will come one by one after each second
 }
