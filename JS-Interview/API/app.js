73dd6121a2aa15efcf9cbf971fb1a396995c7cc1
Shobhika <134936907+shobhikaa16@users.noreply.github.com>
2025-02-14 16:46:31 +0000

    api calls

--- JS-Interview/API/app.js
+++ JS-Interview/API/app.js
@@ -12,26 +12,26 @@
  // www.json.org
  // there are many json validators - where we ca copy and paste the the data from the api call and it eill tell whether data is valid or not
 
-let object = {
-    a:undefined  // undefined is not valid in json
-};
+// let object = {
+//     a:undefined  // undefined is not valid in json
+// };
 
 // Accessing the data from JSON
 // JSON.parse(data) - to parse a string data into a JS object
 
-let jsonResponse =' {"fact":"A cat\u2019s jaw can\u2019t move sideways, so a cat can\u2019t chew large chunks of food.","length":74}'
+// let jsonResponse =' {"fact":"A cat\u2019s jaw can\u2019t move sideways, so a cat can\u2019t chew large chunks of food.","length":74}'
 
-let validResp = JSON.parse(jsonResponse);
-console.log(validResp.fact);
+// let validResp = JSON.parse(jsonResponse);
+// console.log(validResp.fact);
 
 // Another method to convert
 //JSON.stringify(json) - to parse a JS object data into JSON - this we generally when we make our own api
 
-let student={
-    name:"Shobhika",
-    age:22
-}
-JSON.stringify(student);
+// let student={
+//     name:"Shobhika",
+//     age:22
+// }
+// JSON.stringify(student);
 
 //Testing of the API's
 //1.Hppscoth
@@ -41,6 +41,42 @@ JSON.stringify(student);
 // Http Verbs  - Get , POst , Put , Delete
 //Status code - 404 page not found, 200 -ok, 400-bad req, 500 - internal server error and many more
 // Additional information in our url - query String in the url where we can add our query in hte form of key value pair eg. harry+potter
+// request headers and response headers , add additional information to both headers req and res
+
+//Making first API request with javascript
+// let url = "https://catfact.ninja/fact";
+// fetch(url)
+// .then((response) =>{
+//     console.log(response);
+//     return response.json();//this will aso return a promise
+// })
+// .then((data)=>{
+//     console.log(data.fact);
+//     return fetch(url);
+// })
+// .then((res)=>{
+//     return res.json();
+// })
+// .then((data2)=>{
+//     console.log(data2);
+// })
+// .catch((err)=>{
+//     console.log("ERROR",err)
+// })
+
+// using  async and await to form the same function - this format is more readable format
+let url = "https://catfact.ninja/fact";
+async function getFacts(){
+    try{
+        let res = await fetch(url);
+    let data = await res.json();
+    console.log(data.fact);
+}catch(e){
+    console.log("error-",e)
+}
+console.log("Bye!");
+    
+}
 
 
 
