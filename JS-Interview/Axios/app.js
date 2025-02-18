9bdb13a24d159e5a2cd749ac3a76fb1ac7f20494
Shobhika <134936907+shobhikaa16@users.noreply.github.com>
2025-02-18 03:01:00 +0000

    api axios

--- JS-Interview/Axios/app.js
+++ JS-Interview/Axios/app.js
@@ -24,14 +24,51 @@
 // });
 
 // pass headers with the axios
+// const url = "https://icanhazdadjoke.com/";
+// async function getJokes(){
+//     try{
+//         const config = { headers :{Accept:"application/json"}}
+//         let res = await axios.get(url,config);
+//         console.log(res);
+//     }catch(err){
+//         console.log(err);
+//     }
+// }
 
-const url = "https://icanhazdadjoke.com/";
-async function getJokes(){
-    try{
-        const config = { headers :{Accept:"application/json"}}
-        let res = await axios.get(url,config);
-        console.log(res);
-    }catch(err){
-        console.log(err);
+//changing query string with the axios
+
+let url = "http://universities.hipolabs.com/search?name=";
+let btn = document.querySelector("button");
+btn.addEventListener("click",async()=>{
+    let country = document.querySelector("input").value;
+    console.log(country);
+    let colArr = await getCollege(country);
+    console.log(colArr);
+    show(colArr)
+});
+
+function show(colArr){
+    let list = document.querySelector("#list");
+    list.innerText = "";
+    for(col of colArr){
+        console.log(col.name);
+        let li = document.createElement("list");
+        li.innerText = col.name;
+        list.appendChild(li);
     }
 }
+
+
+
+async function getCollege(country){
+    try{
+        let res = await axios.get(url+country);
+        console.log(res.data);
+        return res.data;
+    }catch(e){
+        console.log("error",e);
+        return [] // if any error then return the empty array
+    }
+}
+
+
